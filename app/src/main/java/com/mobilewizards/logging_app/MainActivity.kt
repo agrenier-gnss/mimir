7960803e99a380fd39346ed8f708a156d099efcc
jussilap <petrus.jussila@tuni.fi>
2023-01-23 11:44:20 +0200

    Created setup activity template

--- app/src/main/java/com/mobilewizards/logging_app/MainActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.mobilewizards.logging_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    // Creates main_menu.xml
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.changeParameters -> {
+                val setupIntent = Intent(this@MainActivity, SetupActivity::class.java)
+                startActivity(setupIntent)
+            }
+        }
+        return true
     }
 }
---
+++ app/src/main/java/com/mobilewizards/logging_app/SetupActivity.kt
@@ -0,0 +1,20 @@
+package com.mobilewizards.logging_app
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+
+        val confirmButton = findViewById<Button>(R.id.confirmButton)
+        confirmButton.setOnClickListener{
+            val mainIntent = Intent(this@SetupActivity, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
+    }
+}
