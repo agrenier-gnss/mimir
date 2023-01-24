590e77ebddfa0644be9c5bb1903303a37afb96cf
jussilap <petrus.jussila@tuni.fi>
2023-01-24 16:37:09 +0200

    Created a simple thread function that counts separately from main

--- app/src/main/java/com/mobilewizards/logging_app/MainActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/MainActivity.kt
@@ -2,15 +2,37 @@ package com.mobilewizards.logging_app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.yield
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startButton = findViewById<Button>(R.id.startButton)
+
+        startButton.setOnClickListener {
+            GlobalScope.launch { var count = 0
+                while (count<10) {
+                    count++
+                    Log.println(Log.INFO,"smt", count.toString())
+                    yield()
+                    try {
+                        Thread.sleep(1000)
+                    } catch (ex:Exception) {
+                        Log.println(Log.ERROR,"smt",ex.message.toString())
+                    }
+                }
+            }
+        }
     }
 
     // Creates main_menu.xml
