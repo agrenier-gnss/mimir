4de8d8f60dc57b2c2a7e3ae8628ae04b52002623
Kuitta <Kuitti.anne@gmail.com>
2023-02-09 11:24:23 +0200

    First version of logging BLE
    Basic implementation for logging BLE

--- app/src/main/java/com/mobilewizards/logging_app/ConnectionHandler.kt
+++ app/src/main/java/com/mobilewizards/logging_app/ConnectionHandler.kt
@@ -1,11 +1,15 @@
 package com.mobilewizards.logging_app
 
+import android.annotation.SuppressLint
+import android.bluetooth.*
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.net.wifi.WifiManager
+import android.util.Log
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.net.wifi.WifiManager
-import android.util.Log
 
 class ConnectionHandler {
 
@@ -49,6 +53,42 @@ class ConnectionHandler {
         if (!success) {
             Log.d("Error", "Failed to fetch wifi connections")
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun logBLE() {
+
+        val bluetoothManager= context.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        val bluetoothAdapter = bluetoothManager.adapter as BluetoothAdapter
+        val bluetoothLeScanner = bluetoothAdapter.bluetoothLeScanner
+
+        val scanCallback = object : ScanCallback() {
+
+            override fun onScanResult(callbackType: Int, result: ScanResult?) {
+
+                super.onScanResult(callbackType, result)
+                if(result == null || result.device == null) {
+                    return
+                }
+
+                Log.d("BLE match", result.device.name.toString())
+
+            }
+
+            override fun onBatchScanResults(results: MutableList<ScanResult>?) {
+                Log.d("BLE batch scan result", "Scanning...")
+                super.onBatchScanResults(results)
+            }
+
+            override fun onScanFailed(errorCode: Int) {
+
+                Log.d("Error", "BLE onScanFailed: $errorCode")
+                super.onScanFailed(errorCode)
+            }
+        }
+
+        bluetoothLeScanner.startScan(scanCallback)
+
     }
 
 }
