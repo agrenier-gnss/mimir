a4b52ec5af3a1be116b2bc7133552cb368b86d72
Kuitta <Kuitti.anne@gmail.com>
2023-03-31 17:45:58 +0300

    added toggle button functionality

--- app/src/main/java/com/mobilewizards/logging_app/MauveActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/MauveActivity.kt
@@ -5,13 +5,26 @@ import android.os.Bundle
 import android.widget.Button
 
 class MauveActivity : AppCompatActivity() {
+
+    private var isToggledOn: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mauve)
 
         val loggingButton = findViewById<Button>(R.id.loggingButton)
         loggingButton.setOnClickListener {
-            // Handle button click here
+            isToggledOn = !isToggledOn
+            if (isToggledOn) {
+                findViewById<Button>(R.id.loggingButton).text = "Stop logging"
+            } else {
+                findViewById<Button>(R.id.loggingButton).text = "Start logging"
+            }
+        }
+
+        val downloadButton = findViewById<Button>(R.id.downloadDataButton)
+        downloadButton.setOnClickListener {
+
         }
     }
 }
