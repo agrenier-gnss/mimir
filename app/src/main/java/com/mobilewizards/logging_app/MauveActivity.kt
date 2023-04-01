b7faaf973c854b64399daf2b2015ca669de6851a
Kuitta <Kuitti.anne@gmail.com>
2023-04-01 20:06:45 +0300

    first version of activity changing by sweeping screen

--- app/src/main/java/com/mobilewizards/logging_app/MauveActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/MauveActivity.kt
@@ -1,8 +1,10 @@
 package com.mobilewizards.logging_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
@@ -57,5 +59,49 @@ class MauveActivity : AppCompatActivity() {
         downloadButton.setOnClickListener {
 
         }
+
+        //change to another activity by sweeping
+        var x1 = 0f
+        var y1 = 0f
+        var x2 = 0f
+        var y2 = 0f
+
+        findViewById<View>(R.id.activity_mauve_layout).setOnTouchListener { _, touchEvent ->
+            when (touchEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    x1 = touchEvent.x
+                    y1 = touchEvent.y
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    x2 = touchEvent.x
+                    y2 = touchEvent.y
+                    val deltaX = x2 - x1
+                    val deltaY = y2 - y1
+                    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                        // swipe horizontal
+                        if (Math.abs(deltaX) > 100) {
+                            // left or right
+                            if (deltaX < 0) {
+                                // left swipe
+                                val intent = Intent(this, LogEventActivity::class.java)
+                                startActivity(intent)
+                                true
+                            } else if (deltaX > 0) {
+                                // right swipe
+                                val intent = Intent(this, MainActivity::class.java)
+                                startActivity(intent)
+                                true
+                            }
+                        }
+                    }
+                    // add a default return value of false here
+                    false
+                }
+                else -> false
+            }
+        }
+
     }
+
 }
