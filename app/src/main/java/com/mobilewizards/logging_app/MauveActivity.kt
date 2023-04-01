82885ba7a0af19e2e7f1613703a417ff56ab3075
Kuitta <Kuitti.anne@gmail.com>
2023-04-01 13:43:57 +0300

    finished mauve view functionality

--- app/src/main/java/com/mobilewizards/logging_app/MauveActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/MauveActivity.kt
@@ -2,7 +2,10 @@ package com.mobilewizards.logging_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MauveActivity : AppCompatActivity() {
 
@@ -11,15 +14,43 @@ class MauveActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mauve)
+        supportActionBar?.hide()
 
         val loggingButton = findViewById<Button>(R.id.loggingButton)
+        val dataButton = findViewById<Button>(R.id.downloadDataButton)
+        val loggingText = findViewById<TextView>(R.id.loggingTextView)
+
         loggingButton.setOnClickListener {
-            isToggledOn = !isToggledOn
             if (isToggledOn) {
-                findViewById<Button>(R.id.loggingButton).text = "Stop logging"
-            } else {
                 findViewById<Button>(R.id.loggingButton).text = "Start logging"
+
+                loggingText.text = ""
+
+                loggingButton.animate()
+                    .translationYBy(-250f)
+                    .setDuration(200)
+                    .start()
+
+                Handler().postDelayed({
+                    dataButton.visibility = View.VISIBLE
+                }, 100)
+
+            } else {
+                findViewById<Button>(R.id.loggingButton).text = "Stop logging"
+
+                dataButton.visibility = View.GONE
+
+                loggingButton.animate()
+                    .translationYBy(250f)
+                    .setDuration(500)
+                    .start()
+
+                Handler().postDelayed({
+                    loggingText.text = "Placeholder text ..."
+                }, 300)
+
             }
+            isToggledOn = !isToggledOn
         }
 
         val downloadButton = findViewById<Button>(R.id.downloadDataButton)
