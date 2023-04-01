e0ffa219ded49daa734c9550792c9286cf2a8b01
Kuitta <Kuitti.anne@gmail.com>
2023-04-01 15:55:00 +0300

    Create LogEventActivity.kt

---
+++ app/src/main/java/com/mobilewizards/logging_app/LogEventActivity.kt
@@ -0,0 +1,11 @@
+package com.mobilewizards.logging_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogEventActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logevent)
+    }
+}
