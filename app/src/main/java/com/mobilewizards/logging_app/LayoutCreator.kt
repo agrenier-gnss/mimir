431593a650d1e1ff444615632cc700a4b00e333a
Kuitta <Kuitti.anne@gmail.com>
2023-04-14 13:34:57 +0300

    Create layout presets
    Implementation for dynamically creating layouts from a preset

---
+++ app/src/main/java/com/mobilewizards/logging_app/LayoutCreator.kt
@@ -0,0 +1,15 @@
+package com.mobilewizards.logging_app
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LayoutCreator : AppCompatActivity() {
+
+    fun createMainViewSliderSquare(title: String, frequencyMin: Number, frequencyMax: Number, topConstraint: String) {
+
+    }
+
+    fun createLogEventSquare(title: String, descriptionText: String) {
+
+    }
+
+}
