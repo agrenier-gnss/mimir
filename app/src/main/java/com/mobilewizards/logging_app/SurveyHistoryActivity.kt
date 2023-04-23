e7fdd9a1904e9eff34725334e6d5f2ba118d531b
Kuitta <Kuitti.anne@gmail.com>
2023-04-23 15:52:43 +0300

    Fix not being able to leave activity
    It is possible to return to previous screen when swiping left

--- app/src/main/java/com/mobilewizards/logging_app/SurveyHistoryActivity.kt
+++ app/src/main/java/com/mobilewizards/logging_app/SurveyHistoryActivity.kt
@@ -1,7 +1,10 @@
 package com.mobilewizards.logging_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TableLayout
 
@@ -9,6 +12,7 @@ class SurveyHistoryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_surveyhistory)
+        supportActionBar?.hide()
 
         // Get the parent view to add the TableLayout to
         val parentView = findViewById<ViewGroup>(R.id.container_layout)
@@ -29,6 +33,41 @@ class SurveyHistoryActivity : AppCompatActivity() {
             // Add the TableLayout to the parent view
             parentView.addView(tableLayout)
 
+        }
+
+        var x1 = 0f
+        var y1 = 0f
+        var x2 = 0f
+        var y2 = 0f
+
+        findViewById<View>(R.id.survey_layout).setOnTouchListener { _, touchEvent ->
+            when (touchEvent.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    x1 = touchEvent.x
+                    y1 = touchEvent.y
+                    true
+                }
+                MotionEvent.ACTION_UP -> {
+                    x2 = touchEvent.x
+                    y2 = touchEvent.y
+                    val deltaX = x2 - x1
+                    val deltaY = y2 - y1
+                    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                        // swipe horizontal
+                        if (Math.abs(deltaX) > 100) {
+                            // left or right
+                            if (deltaX > 0) {
+                                // left swipe
+                                val intent = Intent(this, MauveActivity::class.java)
+                                startActivity(intent)
+                                true
+                            }
+                        }
+                    }
+                    false
+                }
+                else -> false
+            }
         }
 
 
